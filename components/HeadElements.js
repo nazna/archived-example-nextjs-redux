c56b84ae830d31886a92c884ddecf23ebbc81b8c
nazna <14846330+nazna@users.noreply.github.com>
2016-12-16 00:23:25 +0900

    feat: add <head> info

---
+++ components/HeadElements.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default () => (
+  <Head>
+    <title>Next.js Demo Application</title>
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>
+);
